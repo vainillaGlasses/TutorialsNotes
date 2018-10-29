718fe20f0c503510d2efec65597ae3136c3477b5
Samantha Arburola <samantha@estudiomanati.com>
2018-10-29 14:19:48 -0600

    redux

---
+++ ReactProject/src/js/reducers/index.js
@@ -0,0 +1,18 @@
+// src/js/reducers/index.js
+
+import { ADD_ARTICLE } from "../constants/action-types";
+
+const initialState = {
+  articles: []
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return { ...state, articles: [...state.articles, action.payload]};
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
